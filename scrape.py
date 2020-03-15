52a439b433e796fd2104b62746037fda7809c2fa
Maharramoff <9804406+Maharramoff@users.noreply.github.com>
2020-03-16 00:15:32 +0400

    add bootstrap

--- scrape.py
+++ scrape.py
@@ -11,6 +11,7 @@ html = """
 <head>
     <meta charset = "UTF-8">
     <title>Confirmed Coronavirus Cases and Deaths by Country</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 {{table | safe}}
