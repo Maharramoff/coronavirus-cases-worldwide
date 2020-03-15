ad649f71811d1ab3e54bd9cf6ce173fc7e290d16
Maharramoff <9804406+Maharramoff@users.noreply.github.com>
2020-03-16 01:51:15 +0400

    table compact style

--- scrape.py
+++ scrape.py
@@ -83,5 +83,5 @@ def index():
     columns = ['Ölkə', 'Xəstələr', 'Yeni Xəstələr', 'Ölüm', 'Yeni Ölüm', 'Sağalıb']
     panda.set_option('display.max_rows', None)
     panda_table = panda.DataFrame(covid.get_result(), columns=columns)
-    return flask.render_template_string (html, table = panda_table.to_html (header = 'true', classes='display', border = 0, justify='left'))
+    return flask.render_template_string (html, table = panda_table.to_html (header = 'true', classes=['display', 'compact'], border = 0, justify='left'))
 
