860d188731bd56b6b5129b1f33139d2ae59a750d
Maharramoff <sh.maharramov@gmail.com>
2020-03-18 22:33:05 +0400

    fix: change datatable id

--- scrape.py
+++ scrape.py
@@ -5,18 +5,19 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 class Coronavirus:
     __check_params = [None, 'All']
     __table_attribute = 'id'
-    __attribute_value = 'main_table_countries'
+    __attribute_value = 'main_table_countries_today'
     __website_url = 'http://www.worldometers.info/coronavirus'
- 
+
     def __init__(self, country=None):
         self.country = country
         html = requests.get(self.__website_url)
         soup = BeautifulSoup(html.text, 'html.parser')
         self.table = soup.find('table', {self.__table_attribute: self.__attribute_value})
- 
+
     def get_result(self):
         trs = self.find_all_trs()
         rows = []
@@ -26,20 +27,21 @@ class Coronavirus:
             if self.country not in self.__check_params and self.country in tds[0]:
                 return [tds[:6]]
         return rows
- 
+
     def find_all_trs(self):
         return self.table.find_all('tr')
- 
+
     @staticmethod
     def __find_all_tds(tr):
         return [td.get_text(strip=True) for td in tr.find_all('td')]
-    
-    
-@app.route('/') 
+
+
+@app.route('/')
 def index():
     covid = Coronavirus('All')
     columns = ['Ölkə', 'Xəstələr', 'Yeni Xəstələr', 'Ölüm', 'Yeni Ölüm', 'Sağalıb']
     panda.set_option('display.max_rows', None)
     panda_table = panda.DataFrame(covid.get_result(), columns=columns)
-    return flask.render_template ('index.html', table = panda_table.to_html (header = 'true', classes=['display', 'compact'], border = 0, justify='left'))
-
+    return flask.render_template('index.html',
+                                 table=panda_table.to_html(header='true', classes=['display', 'compact'], border=0,
+                                                           justify='left'))
