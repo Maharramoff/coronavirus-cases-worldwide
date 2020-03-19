60e1f74d157487855f3f13bbf75a3626c1be4149
Maharramoff <sh.maharramov@gmail.com>
2020-03-19 19:41:50 +0400

    remove cache logic to its class (srp)

--- classes/coronavirus.py
+++ classes/coronavirus.py
@@ -5,6 +5,34 @@ import json
 import time
 
 
+class Cache:
+
+    def __init__(self, file):
+        self.file = file
+
+    def time_diff(self):
+        if os.path.isfile(self.file):
+            last_time = int(os.path.getmtime(self.file))
+            current_time = int(time.time())
+            return current_time - last_time
+        return None
+
+    def set(self, data) -> None:
+        if len(data) == 0:
+            return None
+        os.makedirs(os.path.dirname(self.file), exist_ok=True)
+        with open(self.file, 'w+', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        pass
+
+    def get(self):
+        try:
+            with open(self.file) as json_file:
+                return json.load(json_file)
+        except EnvironmentError:
+            return None
+
+
 class Coronavirus:
     __check_params = [None, 'All']
     __table_attribute = 'id'
@@ -16,11 +44,12 @@ class Coronavirus:
 
     def __init__(self, country=None):
         self.country = country
+        self.cache = Cache(self.__cache_file)
 
     def get_result(self):
-        if None is not self.cache_time_diff() < self.__cache_ttl:
-            return self.get_local_data()
-        table = self.get_remote_data()
+        if None is not self.cache.time_diff() < self.__cache_ttl:
+            return self.__get_local_data()
+        table = self.__get_remote_data()
         trs = self.__find_all_trs(table)
         rows = []
         for tr in trs[1:-1]:
@@ -28,38 +57,16 @@ class Coronavirus:
             rows.append(tds[:6])
             if self.country not in self.__check_params and self.country in tds[0]:
                 return [tds[:6]]
-        self.cache_set(rows)
+        self.cache.set(rows)
         return rows
 
-    def cache_set(self, rows) -> None:
-        if len(rows) == 0:
-            return None
-        os.makedirs(os.path.dirname(self.__cache_file), exist_ok=True)
-        with open(self.__cache_file, 'w+', encoding='utf-8') as f:
-            json.dump(rows, f, ensure_ascii=False, indent=4)
-        pass
-
-    def cache_get(self):
-        try:
-            with open(self.__cache_file) as json_file:
-                return json.load(json_file)
-        except EnvironmentError:
-            return None
-
-    def cache_time_diff(self):
-        if os.path.isfile(self.__cache_file):
-            last_time = int(os.path.getmtime(self.__cache_file))
-            current_time = int(time.time())
-            return current_time - last_time
-        return None
-
-    def get_remote_data(self):
+    def __get_remote_data(self):
         html = requests.get(self.__website_url)
         soup = BeautifulSoup(html.text, 'html.parser')
         return soup.find('table', {self.__table_attribute: self.__attribute_value})
 
-    def get_local_data(self):
-        return self.cache_get()
+    def __get_local_data(self):
+        return self.cache.get()
 
     @staticmethod
     def __find_all_trs(table):
