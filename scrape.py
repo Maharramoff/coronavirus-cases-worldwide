eab2677ac40f37602c2311a82ce09e824b8af364
Shamkhal Maharramov <Maharramoff@users.noreply.github.com>
2020-03-22 14:08:35 +0400

    add covid-19 information page

--- scrape.py
+++ scrape.py
@@ -6,12 +6,18 @@ app = flask.Flask(__name__, static_folder='static')
 
 
 @app.route('/')
-def index():
-    covid = Coronavirus('All')
+def home():
+    covid19 = Coronavirus('All')
     columns = ['Ölkə', 'Xəstələr', 'Yeni Xəstələr', 'Ölüm', 'Yeni Ölüm', 'Sağalıb']
-    rows = covid.get_table_rows()
-    stats = {k: v.replace(',', '') for (k, v) in covid.get_stats().items()}
+    rows = covid19.get_table_rows()
+    stats = {k: v.replace(',', '') for (k, v) in covid19.get_stats().items()}
     return flask.render_template('home/index.html',
                                  stats=stats,
                                  rows=rows,
                                  columns=columns)
+
+
+@app.route('/covid')
+def covid():
+    data = "Covid haqqında"
+    return flask.render_template('covid/index.html', data=data)
