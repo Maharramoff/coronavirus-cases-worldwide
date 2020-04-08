0179922601d233324bb17cf7bed4defd72b6128b
Maharramoff <sh.maharramov@gmail.com>
2020-04-08 22:37:03 +0400

    update: add datetime for news

--- classes/cabmin.py
+++ classes/cabmin.py
@@ -3,6 +3,7 @@ from operator import itemgetter
 import requests
 from bs4 import BeautifulSoup
 from classes import Cache
+import re
 
 
 class Cabmin:
@@ -10,7 +11,7 @@ class Cabmin:
     __stop_num = "675"
     __raw_article = True
     __search_words = ['koronavirus', 'məqsədilə', 'karantin', 'xəstə']
-    __cache_ttl = 10
+    __cache_ttl = 30 * 60
     __cache_file = 'cache/news.json'
 
     def __init__(self, news_limit=20):
@@ -37,6 +38,7 @@ class Cabmin:
             if any(word in html for word in self.__search_words):
                 soup = BeautifulSoup(html, "html.parser")
                 title = soup.find('div', {'class': 'articleTitle'}).text
+                date_time = self.extract_date_time(html)
                 article_num = self.get_article_number(soup)
                 if article_num <= latest_article_num and first_run is False:
                     break
@@ -46,7 +48,7 @@ class Cabmin:
                     article = '<br/>'.join([p.get_text(strip=False).replace(u'\xa0', u' ') for p in paragraphs])
                 if 'style' in article.attrs:
                     del article.attrs['style']
-                row = {'id': article_num, 'title': title, 'body': str(article)}
+                row = {'id': article_num, 'datetime': date_time, 'title': title, 'body': str(article)}
                 cached_data.append(row)
                 found += 1
             self.__page_num += 1
@@ -61,3 +63,12 @@ class Cabmin:
     @staticmethod
     def extract_digit(txt):
         return ''.join(filter(str.isdigit, txt))
+
+    @staticmethod
+    def extract_date_time(txt):
+        dd = r'(0[1-9]|[12]\d|3[01])'
+        months = '(Yanvar|Fevral|Mart|Aprel|May|Iyun|Iyul|Avqust|((Senty|Okty|Noy|Dek)abr))'
+        year = r'[12]\d{3}'
+        hhmm = '([0-1]?[0-9]|2[0-3]):[0-5][0-9]'
+        result = re.search('(' + dd + ' ' + months + ' ' + year + ' - ' + hhmm + ')', txt)
+        return result[0]
